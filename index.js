852c1ffc31a344b748d5c4184814db35b6dfbaa4
Sanidhya Mahajan <sanidhya10628@gmail.com>
2021-05-29 19:56:06 +0530

    commit

--- index.js
+++ index.js
@@ -2,22 +2,33 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const shortUrl = require("node-url-shortener");
-
+app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'/views'));
+
+
 app.get('/', (req,res) => {
     res.render('index');
 })
 
-app.post('/shorten-url', (req,res) => {
+app.post('/',(req,res) => {
     const {url} = req.body;
     const prev = url;
     shortUrl.short(url, function(err, url){
         res.render('short-url',{prev:prev,url:url});
-    });    
+        // res.redirect('/sh');
+    })
 })
+// app.post('/short', (req,res) => {
+//     const {url} = req.body;
+//     const prev = url;
+//     shortUrl.short(url, function(err, url){
+//         res.render('short-url',{prev:prev,url:url});
+//         // res.redirect('/sh');
+//     });    
+// })
 app.listen(8000,() => {
     console.log("On Port 8000");
 })
